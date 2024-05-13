eb8e8903fe537ebd179b4647f5747eda38f1f193
JunehaengLee <165055562+amsul73@users.noreply.github.com>
2024-05-14 00:49:37 +0900

    Revert "[TC-1] 회원가입 페이지 구현"

--- frontend/README.md
+++
@@ -1,7 +0,0 @@
-[React 설치]
-npx create-react-app [project]
-
-[모듈 설치]
-npm i axios
-npm i react-cookies
-npm i http-proxy-middleware
--- frontend/src/component/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Footer(props) {
-    return (
-        <div>All Copyright Reserved. 2023 ⓒ GachaPEDIA</div>
-    );
-}
-
-export default Footer;
